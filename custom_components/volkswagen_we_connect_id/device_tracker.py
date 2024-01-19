e97925d97aa8d3f572b8bb9c8e5c28f0376b1357
Jarno <2635154+sliepie@users.noreply.github.com>
2024-01-19 08:54:48 +0000

    Update weconnect

--- custom_components/volkswagen_we_connect_id/device_tracker.py
+++ custom_components/volkswagen_we_connect_id/device_tracker.py
@@ -1,22 +1,15 @@
 """
 Support for Volkswagen WeConnect Platform
 """
-import logging
 
 from weconnect import weconnect
 
-from homeassistant.components.device_tracker import SourceType.GPS
+from homeassistant.components.device_tracker import SourceType
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from homeassistant.util import slugify
 
 from . import VolkswagenIDBaseEntity, get_object_value
 from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Add sensors for passed config_entry in HA."""
@@ -30,7 +23,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     entities = []
 
-    for index, vehicle in enumerate(coordinator.data):
+    for index in enumerate(coordinator.data):
         entities.append(VolkswagenIDSensor(we_connect, coordinator, index))
 
     if entities:
